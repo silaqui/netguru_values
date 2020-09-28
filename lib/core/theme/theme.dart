3fd0e20e15274d389f44aba640754d8d4bbbde88
Miki <aivi.pol@gmail.com>
2020-09-28 17:19:47 +0200

    Add comfortaa font for theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 final lightTheme = ThemeData(
   primarySwatch: Colors.green,
   visualDensity: VisualDensity.adaptivePlatformDensity,
+  fontFamily: 'Comfortaa',
 );
 
-final darkTheme = ThemeData.dark();
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  fontFamily: 'Comfortaa',
+);
